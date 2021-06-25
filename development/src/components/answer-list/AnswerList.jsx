6aed525e8c7c030e370826aeb9d50bffcc27ec36
Xelxeim <xelxeim@gmail.com>
2021-06-25 08:29:57 +0300

    Fix shuffle bug

--- development/src/components/answer-list/AnswerList.jsx
+++ development/src/components/answer-list/AnswerList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import AnswerItem from "../answer-item";
 
 import functions from "../../services/functions";
@@ -6,14 +6,14 @@ import functions from "../../services/functions";
 import './AnswerList.scss';
 
 const AnswerList = ({ correctAnswer, incorrectAnswers, type, currentNumber, onChangeHandler }) => {
-  const [userAnswers, setUserAnswers] = useState([]);
-  const allAnswers = [...incorrectAnswers, correctAnswer];
+  const [userAnswers, setUserAnswers] = useState([]); // this state not using now, but may be useful in the future. Now this state controls rerendering. 
+  const allAnswers = useMemo(() => [...incorrectAnswers, correctAnswer], [incorrectAnswers, correctAnswer]);
 
   useEffect(() => {
     functions.shuffle(allAnswers);
     setUserAnswers(allAnswers);
     functions.dropCheckboxes("btn-element");
-  }, [currentNumber])
+  }, [currentNumber, allAnswers])
 
   return (
     <div className="quiz__answer-list">
